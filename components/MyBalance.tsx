40e60e0d4bea6bcbb8e269d657f7d3f13b967e57
Rupam Shil <rupamshil111@gmail.com>
2025-01-09 15:38:47 +0530

    (feat) added wallet connect

--- components/MyBalance.tsx
+++ components/MyBalance.tsx
@@ -1,9 +1,29 @@
+import { useAccount, useBalance } from '@starknet-react/core';
 import React from 'react';
 
 function MyBalance() {
+	const { address: userAddress } = useAccount();
+
+	const {
+		isLoading: balanceIsLoading,
+		isError: balanceIsError,
+
+		data: balanceData,
+	} = useBalance({
+		address: userAddress,
+		watch: true,
+		token: '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d',
+	});
+
 	return (
 		<div className="bg-card rounded-2xl p-4 flex flex-col">
-			<h3 className="text-white">My Balance</h3>
+			{!balanceIsLoading && !balanceIsError && userAddress && (
+				<div className="p-4 bg-white border-black border">
+					<h3 className="text-lg font-bold mb-2">Your Balance</h3>
+					<p>Symbol: {balanceData?.symbol}</p>
+					<p>Balance: {Number(balanceData?.formatted).toFixed(4)}</p>
+				</div>
+			)}
 		</div>
 	);
 }
